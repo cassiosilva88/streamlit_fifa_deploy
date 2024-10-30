1f1ddc5d9c653f3ee8511d7c48cf3ab42e5adb62
Cassio Silva <cassiosilva0888@gmail.com>
2024-10-29 22:12:54 -0300

    Update 1_🏠_home.py

--- 1_🏠_home.py
+++ 1_🏠_home.py
@@ -11,7 +11,7 @@ if "data" not in st.session_state:
     st.session_state["data"] = df_data
 
 st.markdown("# FIFA23 OFFICIAL DATASET! ⚽️")
-st.sidebar.markdown("Desenvolvido por [Asimov Academy](https://asimov.academy)")
+st.sidebar.markdown("Desenvolvido por Cassio Silva")
 
 
 btn = st.link_button(
